9b382e7ca691a40d3f10a193e1f22e93df3e8865
spefalex <rakoto.tsira@gmail.com>
2017-09-21 11:41:01 +0300

    test change

--- api/services/jwToken.js
+++ api/services/jwToken.js
@@ -6,9 +6,9 @@ var
 module.exports.issue = function(payload) {
   return jwt.sign(
     payload,
-    tokenSecret, // Token Secret that we sign it with
+    tokenSecret, // Token Secret que nous signons avec
     {
-        expiresIn : 60*60*24 // Token Expire time
+        expiresIn : 60*60*24 // Token Expire 
     }
   );
 };
@@ -16,9 +16,9 @@ module.exports.issue = function(payload) {
 // Verifies token on a request
 module.exports.verify = function(token, callback) {
   return jwt.verify(
-    token, // The token to be verified
-    tokenSecret, // Same token we used to sign
-    {}, // No Option, for more see https://github.com/auth0/node-jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
-    callback //Pass errors or decoded token to callback
+    token, // vérification token
+    tokenSecret, 
+    {}, 
+    callback 
   );
 };
